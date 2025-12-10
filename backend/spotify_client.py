ee5ce2cdfc07c1d86692e86b572046c32a5e3cf5
Sean Ma <sean.ma711@gmail.com>
2025-12-10 15:33:11 -0500

    clean slate

--- backend/spotify_client.py
+++
@@ -1,9 +0,0 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id="YOUR_CLIENT_ID",
-    client_secret="YOUR_CLIENT_SECRET",
-    redirect_uri="http://localhost:5000/callback",
-    scope="user-library-read"
-))
